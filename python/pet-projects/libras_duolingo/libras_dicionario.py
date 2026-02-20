e34434ffe074071b84d9c6b9b4a8184a99490433
Fellipe Freire Oliveira <freirefellipe@gmail.com>
2026-02-20 17:35:43 -0300

    atualização do dicionário de Duolingo em Libras, pasta pet-projects.

--- python/pet-projects/libras_duolingo/libras_dicionario.py
+++ python/pet-projects/libras_duolingo/libras_dicionario.py
@@ -1,14 +1,13 @@
-verbo = ['comer', 'esquecer', 'encontrar', 'entender', 'aprender', 'querer', 'poder', 'fazer', 'falar' 'ver', 'criar', 'descansar', 'ensinar', 'brincar', 'dar', 'passear', 'agendar', 'precisar', 'assistir', '(não) gostar', 'gostar muito', 'sair']
+verbo = ['comer', 'beber', 'cozinhar', 'esquecer', 'encontrar', 'entender', 'aprender', 'querer', 'poder', 'fazer', 'falar', 'ver', 'criar', 'descansar', 'ensinar', 'brincar', 'dar', 'passear', 'agendar', 'precisar', 'assistir', '(não) gostar', 'gostar muito', 'amar', 'odiar', 'sair', 'nascer', 'viajar', 'chover', 'ir', 'vir', 'abandonar', 'desistir', 'ignorar', 'cuidar', 'casar', 'ter']
 
 pronome = ['eu', 'tu', 'ele', 'ela', 'nós', 'eles', 'elas']
 
-adjetivo = ['grande', 'pequeno', 'azul', 'vermelho', 'verde', 'amarelo', 'roxo', 'laranja', 'rosa', 'cinza', 'rápido', 'bom', 'novo', 'sozinha/o']
+adjetivo = ['grande', 'pequeno', 'pouco', 'azul', 'vermelho', 'verde', 'amarelo', 'roxo', 'laranja', 'rosa', 'cinza', 'rápido', 'bom', 'novo', 'sozinha/o', 'muito', 'agradável']
 
-substantivo = ['parque', 'assunto', 'carro', 'moto', 'bicicleta', 'banco', 'casa', 'igreja', 'tempo', 'professor', 'semana', 'exemplo', 'frase', 'agenda', 'dentista', 'cachorro', 'prova', 'faculdade','esposo/a', 'filme', 'televisão', 'estrelas', 'fazenda']
+substantivo = ['família', 'masculino', 'feminino', 'mãe', 'pai', 'filha/o', 'grávida', 'irmã(o)', 'neto', 'avô/ó', 'parque', 'assunto', 'carro', 'moto', 'bicicleta', 'banco', 'casa', 'igreja', 'tempo/temporada/clima', 'professor', 'semana', 'exemplo', 'frase', 'agenda', 'dentista', 'cachorro', 'prova', 'faculdade','esposo/a', 'filme', 'televisão', 'estrelas', 'fazenda', 'mês', 'chuva', 'grupos']
 
-adverbio = ['sempre', 'anteontem', 'ontem', 'hoje', 'amanhã', 'dia', 'tarde', 'noite', 'madrugada', 'agora', 'manhã', 'passado', 'futuro', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo']
+adverbio = ['sempre', 'anteontem', 'ontem', 'hoje', 'amanhã', 'dia', 'tarde', 'noite', 'madrugada', 'agora', 'manhã', 'passado', 'futuro', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo', 'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro', 'primavera', 'verão', 'outono', 'inverno']
 
 conectivo = ['também', 'ou', 'mas', 'porque', 'com']
 
-
-print()
+interrogativo = ['como?', 'quando?', 'onde?', 'o quê?', 'quem?', 'por quê?']
